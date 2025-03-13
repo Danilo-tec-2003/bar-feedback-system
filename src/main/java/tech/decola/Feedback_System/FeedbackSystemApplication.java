d48c50f945bc28319ca70b10310503f1be54713b
Danilo Mendes <danilodev.br@gmail.com>
2025-03-12 22:29:30 -0300

    Partial implementation

---
+++ src/main/java/tech/decola/Feedback_System/FeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package tech.decola.Feedback_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackSystemApplication.class, args);
+	}
+
+}
